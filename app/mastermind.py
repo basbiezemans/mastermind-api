18123af8d38224ef6daa1df61ec2fc4b6209b40f
Bas Biezemans <bas.biezemans@gmail.com>
2018-11-28 15:13:56 +0100

    Codemaker feedback

--- app/mastermind.py
+++ app/mastermind.py
@@ -1,13 +1,39 @@
+from random import randint
 
 class CodeMaker:
-    def __init__(self):
-        pass
+    def __init__(self, code=None):
+        if code is None:
+            self.initialize()
+        else:
+            self.pattern = [int(c) for c in code]
+
+    def initialize(self):
+        """ Creates a random pattern of four digits between 1 and 6 (inclusive)
+        """
+        self.pattern = [randint(1,6) for _ in range(4)]
+
+    def evaluate(self, guess):
+        """ Evaluates the guess and returns a list of ones and zeros for correct digits
+        """
+        digits = [int(c) for c in guess.code]
+        result = []
+        pattern = self.pattern[:]
+        for i in range(4):
+            digit = digits[i]
+            if digit == self.pattern[i]:
+                result.insert(0, 1)
+                pattern.remove(digit)
+            elif digit in pattern:
+                result.append(0)
+                pattern.remove(digit)
+        return result
 
     def feedback(self, guess):
         """ Returns a Feedback object or raises a ValueError
         """
         if guess.is_valid():
-            return Feedback(guess.code, [])
+            result = self.evaluate(guess)
+            return Feedback(guess.code, result)
         else:
             raise ValueError('Invalid Guess')
 
@@ -33,7 +59,19 @@ class Guess:
     def is_valid(self):
         """ Returns True if this is a valid guess and False otherwise
         """
-        return True if self.code == '1234' else False
+        if self.code is None:
+            return False
+        if len(self.code) != 4:
+            return False
+        try:
+            int(self.code)
+        except ValueError:
+            return False
+        for c in self.code:
+            n = int(c)
+            if n < 1 or n > 6:
+                return False
+        return True
 
     def __repr__(self):
         return 'Guess(code={self.code})'
--- tests/test_mastermind.py
+++ tests/test_mastermind.py
@@ -34,3 +34,22 @@ class TestMastermind(TestCase):
     def test_guess_has_incorrect_pattern(self):
         guess = Guess('1238')
         self.assertEqual(guess.is_valid(), False)
+
+    def test_codemaker_provides_correct_feedback(self):
+        codemaker = CodeMaker(code='6243')
+        feedback = codemaker.feedback(Guess('1234'))
+        # 2, 3 and 4 are correct, 2 also has the correct position
+        expected = [1,0,0]
+        self.assertEqual(feedback.response, expected)
+
+    def test_codemaker_provides_correct_feedback_if_there_are_duplicates(self):
+        # If there are duplicate colours in the guess, they cannot all be awarded a key peg unless 
+        # they correspond to the same number of duplicate colours in the hidden code.
+        codemaker = CodeMaker(code='6243')
+        feedback = codemaker.feedback(Guess('6225'))
+        expected = [1,1]
+        self.assertEqual(feedback.response, expected)
+        codemaker = CodeMaker(code='6443')
+        feedback = codemaker.feedback(Guess('4124'))
+        expected = [0,0]
+        self.assertEqual(feedback.response, expected)
