93bf42506b193a6b5181e79fd38cfa971b1a32f8
Bas Biezemans <bas.biezemans@gmail.com>
2018-12-02 10:38:41 +0100

    Refactoring

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,37 +4,38 @@ from requests import get, put
 class TestAPI(TestCase):
 
     def setUp(self):
-        game = get('http://localhost:5000/create/')
-        self.token = game.json().get('token')
+        self.create_uri = 'http://localhost:5000/create/'
+        self.update_uri = 'http://localhost:5000/guess/'
+        self.token = get(self.create_uri).json().get('token')
 
     def test_create_game(self):
-        test = get('http://localhost:5000/create/')
+        test = get(self.create_uri)
         self.assertEqual(test.status_code, 201)
 
     def test_game_token(self):
-        test = get('http://localhost:5000/create/')
+        test = get(self.create_uri)
         token = test.json().get('token', 'NA')
-        test = put('http://localhost:5000/guess/', data={
+        test = put(self.update_uri, data={
             'code': '1234',
             'token': token
         })
         self.assertEqual(test.json().get('token'), token)
 
     def test_guess_without_token(self):
-        test = put('http://localhost:5000/guess/', data={
+        test = put(self.update_uri, data={
             'code': '1234'
         })
         self.assertEqual(test.status_code, 403)
 
     def test_guess_without_code(self):
-        test = put('http://localhost:5000/guess/', data={
+        test = put(self.update_uri, data={
             'token': self.token
         })
         self.assertEqual(test.status_code, 400)
 
     def test_guess_with_code_and_token(self):
         code = '1234'
-        test = put('http://localhost:5000/guess/', data={
+        test = put(self.update_uri, data={
             'code': code,
             'token': self.token
         })
